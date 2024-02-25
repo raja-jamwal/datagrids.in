690fa9428b5e7a2a5e176b940722a71902123001
Raja Jamwal <linux1@zoho.com>
2024-02-25 19:55:19 +0530

    authenticate on error

--- src/app/notion/error.tsx
+++ src/app/notion/error.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Error({
   error,
@@ -10,14 +11,18 @@ export default function Error({
   error: Error & { digest?: string };
   reset: () => void;
 }) {
+  const router = useRouter();
   useEffect(() => {
     console.error(error);
   }, [error]);
 
   return (
-    <div>
+    <div className="flex flex-col space-y-2">
       <h2>{error.message}</h2>
       <Button onClick={() => reset()}>Try again</Button>
+      <Button onClick={() => router.push("/api/notion_oauth")}>
+        Authenticate
+      </Button>
     </div>
   );
 }

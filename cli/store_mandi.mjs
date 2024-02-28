ddfdd1362122e560ad2840e7f647cade4c4a7c92
Raja Jamwal <linux1@zoho.com>
2024-02-28 10:57:33 +0530

    Remove "type" attribute from package.json and rename file.

--- cli/store_mandi.mjs
+++ cli/store_mandi.mjs
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 import fetch from "node-fetch";
-import { eachDayOfInterval, format, parseISO } from "date-fns";
+import {eachDayOfInterval, format, parseISO} from "date-fns";
 
 async function callApi(date) {
   const formatDate = format(date, "yyyy-MM-dd");
   const response = await fetch(
-    `http://localhost:3000/api/mandi/save?date=${formatDate}`,
+      `http://localhost:3000/api/mandi/save?date=${formatDate}`,
   );
   const data = await response.text();
   console.log(`Response for date ${formatDate}: ${data}`);
@@ -15,7 +15,7 @@ async function callApi(date) {
 function main() {
   if (process.argv.length !== 4) {
     console.error(
-      "Please provide from and to dates, e.g. cli/store_mandis.js 2021-01-01 2021-01-31",
+        "Please provide from and to dates, e.g. cli/store_mandis.mjs 2021-01-01 2021-01-31",
     );
     process.exit(1);
   }
@@ -23,7 +23,7 @@ function main() {
   const fromDate = parseISO(process.argv[2]);
   const toDate = parseISO(process.argv[3]);
 
-  const dateInterval = eachDayOfInterval({ start: fromDate, end: toDate });
+  const dateInterval = eachDayOfInterval({start: fromDate, end: toDate});
   console.log(`Fetching data for ${dateInterval.length} days`);
 
   for (const date of dateInterval) {
